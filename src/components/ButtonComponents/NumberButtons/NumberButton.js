42734dfebfc2855c3e01ffc88016f41537e7cd29
April Ding <aprilding@Aprils-iMac.local>
2019-11-05 18:26:25 -0500

    week7day2Project

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
+  
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      {/* Display a button element rendering the data being passed down from the parent container on props */
+      
+      <button className={`btn ${props.style}`} onClick={()=>props.fun}>{props.char}</button>
+      
+      }
+
     </>
   );
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React,{useState} from "react";
+import SpecialButton from "./SpecialButton";
+import {specials} from "../../../data";
 
 //import any components needed
 
@@ -6,12 +8,20 @@ import React from "react";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
-
+  const [Spec,setSpec] = useState(specials);
   return (
-    <div>
+    <div className="all-spec-btn">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+       
+       Spec.map((item,index)=>(
+         <SpecialButton key = {index} char ={item} />
+       ))
+
+       }
     </div>
   );
 };
+
+export default Specials;
